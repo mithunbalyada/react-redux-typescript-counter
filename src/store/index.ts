ea520c2e8e52fc4b64028083a59f8096cc2082bb
Mithun <mithunbalyada@gmail.com>
2020-03-31 19:09:34 +0100

    adding redux-devtools extension

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,14 @@
-import { combineReducers, createStore, applyMiddleware } from "redux";
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
 import counterReducer from "./counter/reducer";
 import {ICounterState} from './counter/types';
 import logger from './logger';
+
+declare global {
+    interface Window {
+      __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
+
 
 const reducer = combineReducers({
     counter: counterReducer
@@ -11,7 +18,12 @@ export interface IState {
     counter: ICounterState
 }
 
+/* 
+Adding DevTools extension to watch Redux store on chrome DevTools
+Reference: https://github.com/zalmoxisus/redux-devtools-extension
+*/
+const composeEnhancers  = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, applyMiddleware(logger));
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
 
 export default store;
